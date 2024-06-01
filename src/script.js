873a9eb43786494d1f3b55132dfa26b1e57d8bdf
Victor Schwarz Borges <160352651+Xuais@users.noreply.github.com>
2024-05-31 22:04:41 -0300

    começo do processo de codificação

---
+++ src/script.js
@@ -0,0 +1,16 @@
+# 1️⃣ Desafio Classificador de nível de Herói
+
+let personagem = "Zeca Machadinho"
+let experiencia = 1005
+let nivelXp = ""
+
+
+if (experiencia <= 1000){
+  nivelXp = "Ferro"
+
+} else if (experiencia >= 1001 && experiencia <= 2000){
+  nivelXp = "Bronze"
+ 
+}
+
+	console.log("Seu nível, " + personagem + " é: " + nivelXp);v
