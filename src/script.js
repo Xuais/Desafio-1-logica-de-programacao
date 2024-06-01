c7bff7bc51f8709b0866a51b1bd237726f480c9f
Victor Schwarz Borges <160352651+Xuais@users.noreply.github.com>
2024-05-31 22:17:28 -0300

    correção no último Else para Else If, código rodando corretamente

--- src/script.js
+++ src/script.js
@@ -1,35 +1,35 @@
 // 1️⃣ Desafio Classificador de nível de Herói
 
-let personagem = "Zeca Machadinho"
-let experiencia = 1005
-let nivelXp = ""
+let personagem = "Zeca Machadinho";
+let experiencia = 1005;
+let nivelXp = "";
 
 
 if (experiencia <= 1000){
-  nivelXp = "Ferro"
+  nivelXp = "Ferro";
 
 } else if (experiencia >= 1001 && experiencia <= 2000){
-  nivelXp = "Bronze"
+  nivelXp = "Bronze";
  
 } else if (experiencia >= 2001 && experiencia <= 5000){
-    nivelXp = "Prata"
+    nivelXp = "Prata";
 
 } else if (experiencia >= 5001 && experiencia <= 7000){
-    nivelXp = "Ouro"
+    nivelXp = "Ouro";
 
 } else if (experiencia >= 7001 && experiencia <= 8000){
-    nivelXp = "Platina"
+    nivelXp = "Platina";
 
 } else if (experiencia >= 8001 && experiencia <= 9000){
-    nivelXp = "Ascendente"
+    nivelXp = "Ascendente";
 
 } else if (experiencia >= 9001 && experiencia <= 10000){
-    nivelXp = "Imortal"
+    nivelXp = "Imortal";
  
-} else (experiencia >= 10001){
-    nivelXp = "Radiante"
-}
+} else if (experiencia >= 10001){
+    nivelXp = "Radiante";
+};
    
-console.log("Seu nível, " + personagem + ", é: " + nivelXp);
+console.log("Seu nível, " + personagem + ", é: " + nivelXp);
 
 // fim 
